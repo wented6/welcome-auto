83cb407282124883845ab9ff77c18ae7e154ad76
wented6 <44713416+wented6@users.noreply.github.com>
2018-12-17 03:24:42 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js'),
+    meercy = new Discord.Client({sisableEveryone: true})
+    console.log("oNNN");
+meercy.on('guildMemberAdd', member => {
+const fuli= member.guild.channels.get("515944328921874432");
+if(!fuli) return;
+if(fuli) {
+setTimeout(() => fuli.send(`` 𝐖elcome 𝐓o , **Universe.**`), 4000)
+}
+});
+meercy.login(process.env.WE);
