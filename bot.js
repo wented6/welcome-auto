afc81f5d12cdba005ddb2add5cc3a72dbe450c73
wented6 <44713416+wented6@users.noreply.github.com>
2018-12-19 18:44:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,15 @@
 const D1 = require('discord.js');
 const D2 = require('discord.js');
 const D3 = require('discord.js');
+const D4 = require('discord.js');
+const D5 = require('discord.js');
 
 
 const p1 = new D1.Client();
 const p2 = new D2.Client();
 const p3 = new D3.Client();
+const p4 = new D4.Client();
+const p5 = new D5.Client();
 
 
 
@@ -41,6 +45,28 @@ p3.on('guildMemberAdd', member => {
 });
 
 
+      console.log("oNNN4");
+p4.on('guildMemberAdd', member => {
+  const fuli= member.guild.channels.get("515944328921874432");
+  if(!fuli) return;
+  if(fuli) {
+  setTimeout(() => fuli.send(`**Welcُme To** Uni   **#**), 4000)
+}
+});
+
+
+
+
+      console.log("oNNN5");
+p5.on('guildMemberAdd', member => {
+  const fuli= member.guild.channels.get("515944328921874432");
+  if(!fuli) return;
+  if(fuli) {
+  setTimeout(() => fuli.send(𝐖elcome 𝐓o , **Universe.**), 4000)
+}
+});
+
+
 
 
 
@@ -51,3 +77,5 @@ p3.on('guildMemberAdd', member => {
 p1.login(process.env.B1);
 p2.login(process.env.B2);
 p3.login(process.env.B3);
+p4.login(process.env.B4);
+p5.login(process.env.B5);
