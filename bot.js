a7fcb281fad506a1199f0ba1dcb4893690c4e5c4
wented6 <44713416+wented6@users.noreply.github.com>
2018-12-25 15:12:19 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,12 +2,15 @@ const D1 = require('discord.js');
 const D2 = require('discord.js');
 const D3 = require('discord.js');
 const D4 = require('discord.js');
+const D5 = require('discord.js');
+
 
 
 const p1 = new D1.Client();
 const p2 = new D2.Client();
 const p3 = new D3.Client();
 const p4 = new D4.Client();
+const p5 = new D5.Client();
 
 
 
@@ -43,8 +46,18 @@ p3.on('guildMemberAdd', member => {
 }
 });
 
-      console.log("oNNN3");
+      console.log("oNNN4");
 p4.on('guildMemberAdd', member => {
+  const fuli= member.guild.channels.get("515175025016307722");
+  if(!fuli) return;
+  if(fuli) {
+  setTimeout(() => fuli.send(`**Benvenuto alla Peace ..**`), 4000)
+}
+});
+
+
+      console.log("oNNN5");
+p5.on('guildMemberAdd', member => {
   const fuli= member.guild.channels.get("515175025016307722");
   if(!fuli) return;
   if(fuli) {
@@ -67,4 +80,5 @@ p1.login(process.env.B1);
 p2.login(process.env.B2);
 p3.login(process.env.B3);
 p4.login(process.env.B4);
+p5.login(process.env.B4);
 
