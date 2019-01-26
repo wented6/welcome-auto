0429e42c3b3f688a0dcc1ca2a437e86465623cde
wented6 <44713416+wented6@users.noreply.github.com>
2019-01-26 17:36:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,10 @@
 const D1 = require('discord.js');
-const D2 = require('discord.js');
-const D3 = require('discord.js');
-const D4 = require('discord.js');
-const D5 = require('discord.js');
-const D6 = require('discord.js');
 
 
 
 
 const p1 = new D1.Client();
-const p2 = new D2.Client();
-const p3 = new D3.Client();
-const p4 = new D4.Client();
-const p5 = new D5.Client();
-const p6 = new D6.Client();
+
 
 
 
@@ -21,62 +12,10 @@ const p6 = new D6.Client();
 
     console.log("oNNN1");
 p1.on('guildMemberAdd', member => {
-const fuli= member.guild.channels.get("515175025016307722");
+const fuli= member.guild.channels.get("490622145605402661");
 if(!fuli) return;
 if(fuli) {
-setTimeout(() => fuli.send(`**Benvenuto alla Peace ..**`), 4000)
-}
-});
-
-
-
-    console.log("oNNN2");
-p2.on('guildMemberAdd', member => {
-const fuli= member.guild.channels.get("515175025016307722");
-if(!fuli) return;
-if(fuli) {
-setTimeout(() => fuli.send(`**Benvenuto alla Peace ..**`), 4000)
-}
-});
-
-
-
-      console.log("oNNN3");
-p3.on('guildMemberAdd', member => {
-  const fuli= member.guild.channels.get("515175025016307722");
-  if(!fuli) return;
-  if(fuli) {
-  setTimeout(() => fuli.send(`**Benvenuto alla Peace ..**`), 4000)
-}
-});
-
-      console.log("oNNN4");
-p4.on('guildMemberAdd', member => {
-  const fuli= member.guild.channels.get("515175025016307722");
-  if(!fuli) return;
-  if(fuli) {
-  setTimeout(() => fuli.send(`**Benvenuto alla Peace ..**`), 4000)
-}
-});
-
-
-      console.log("oNNN5");
-p5.on('guildMemberAdd', member => {
-  const fuli= member.guild.channels.get("515175025016307722");
-  if(!fuli) return;
-  if(fuli) {
-  setTimeout(() => fuli.send(`**Benvenuto alla Peace ..**`), 4000)
-}
-});
-
-
-
-      console.log("oNNN6");
-p6.on('guildMemberAdd', member => {
-  const fuli= member.guild.channels.get("515175025016307722");
-  if(!fuli) return;
-  if(fuli) {
-  setTimeout(() => fuli.send(`**Benvenuto alla Peace ..**`), 4000)
+setTimeout(() => fuli.send(`**Welcome To **Horizon's** Server ..`), 4000)
 }
 });
 
@@ -91,11 +30,5 @@ p6.on('guildMemberAdd', member => {
 
 
 
-p1.login(process.env.B1);
-p2.login(process.env.B2);
-p3.login(process.env.B3);
-p4.login(process.env.B4);
-p5.login(process.env.B5);
-p6.login(process.env.B6);
 
-
+p1.login(process.env.B);
