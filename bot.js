23a6fffccf04c5f5376b4bbf6677ecf3cd651db1
wented6 <44713416+wented6@users.noreply.github.com>
2019-01-26 17:55:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ const p1 = new D1.Client();
 
     console.log("oNNN1");
 p1.on('guildMemberAdd', member => {
-const fuli= member.guild.channels.get("490622145605402661");
+const fuli= member.guild.channels.get("534460862027530263");
 if(!fuli) return;
 if(fuli) {
 setTimeout(() => fuli.send(`**Welcome To **Horizon's** Server ..`), 4000)
